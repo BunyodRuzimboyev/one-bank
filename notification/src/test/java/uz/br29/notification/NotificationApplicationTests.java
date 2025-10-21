6555d2566d3f7a6964c52f87b5da4b2d6faa5d46
Bunyod Ruzimboyev <=>
2025-10-21 18:13:26 +0500

    Copy microservice aplication default files.
    I have copied full project. It is divided 4 service

---
+++ notification/src/test/java/uz/br29/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.br29.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
